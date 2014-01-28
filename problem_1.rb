cc7c67ef3cae383c7433be0a535916ec73fdca76
My Name <dodd.jl@gmail.com>
2014-01-28 10:11:59 -0500

    problem 1

--- problem_1.rb
+++ problem_1.rb
@@ -7,7 +7,6 @@ multiple_one = gets.chomp.to_i
 puts "Second multiple to select by:"
 multiple_two = gets.chomp.to_i
 
-
 def range_sum(range_start, range_end, multiple_one, multiple_two)
 
   range_array = *(range_start...range_end)
@@ -16,12 +15,8 @@ def range_sum(range_start, range_end, multiple_one, multiple_two)
     (number % multiple_one == 0) || (number % multiple_two == 0)
   end
 
-  sum_of = selected_numbers.reduce(:+)
-
-  puts sum_of
+  return selected_numbers.reduce(:+)
 
 end
 
-
-
-range_sum(range_start, range_end, multiple_one, multiple_two)
+puts range_sum(range_start, range_end, multiple_one, multiple_two)
